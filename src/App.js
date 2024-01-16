e9ccb3679045f97387b17b9992b2fdc54ab639fb
dragen62 <125238620+dragen62@users.noreply.github.com>
2024-01-17 03:40:21 +0700

    Added simple choose origin airport by IATA

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ const API_KEY = 'b5b4489dfeae21f1c97508b7df14534e';
 class App extends React.Component {
   gettingTickets = async (event) => {
     event.preventDefault();
-    const api_url = await fetch(`https://cors-anywhere.herokuapp.com/api.travelpayouts.com/aviasales/v3/prices_for_dates?origin=MOW&destination=DXB&departure_at=2024-07&return_at=2024-08&unique=false&sorting=price&direct=false&cy=rub&limit=30&page=1&one_way=true&token=${API_KEY}`);
+    const city = event.target.elements.city.value;
+    const api_url = await fetch(`https://cors-anywhere.herokuapp.com/api.travelpayouts.com/aviasales/v3/prices_for_dates?origin=MOW&destination=${city}&departure_at=2024-07&return_at=2024-08&unique=false&sorting=price&direct=false&cy=rub&limit=30&page=1&one_way=true&token=${API_KEY}`);
     const data = await api_url.json();
     console.log(data);
   }
